6465dd8e1ea0c34bc448fe28e0b72e0991c46cdf
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-01-23 08:48:25 -0700

    Started my first C++ project

---
+++ FirstCPlusPlus/FirstCPlusPlus/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Wilsher, Ethan on 1/23/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    string word;
+    getline(cin, word);
+    // cin >> word;
+    cin >> word;
+    cout << "Hello " << word << endl;
+    return 17;
+}
+
