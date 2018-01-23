9fed91524a64ca35497978eda460792d82cdaaad
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-01-23 08:58:41 -0700

    Added a new group with a runner and controller file.

---
+++ FirstCPlusPlus/FirstCPlusPlus/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  FirstCPlusPlus
+//
+//  Created by Wilsher, Ethan on 1/23/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ FirstCPlusPlus/FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Wilsher, Ethan on 1/23/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#include <stdio.h>
