42398e6b6d92ceda3ea090a2ae5251b304da69be
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-02-02 09:06:14 -0700

    Adding code for a template array

---
+++ DataStructuresProject/DataStructuresProject/Controller/Model/Structures/Array.cpp
@@ -0,0 +1,9 @@
+//
+//  Array.cpp
+//  DataStructuresProject
+//
+//  Created by Wilsher, Ethan on 2/2/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#include "Array.hpp"
---
+++ DataStructuresProject/DataStructuresProject/Controller/Model/Structures/Array.hpp
@@ -0,0 +1,112 @@
+//
+//  Array.hpp
+//  DataStructuresProject
+//
+//  Created by Wilsher, Ethan on 2/2/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class Array
+{
+private:
+    Type * internalArray;
+    int size;
+public:
+    Array<Type>(int size);
+    
+    Array<Type>(const Array<Type> & toCopy);
+    
+    ~Array<Type>();
+    
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+}
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    
+    internalArray = new Type[size];
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
+
+template <class Type>
+Array<Type> :: ~Array()
+{
+    delete [] internalArray;
+}
+
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign != this)
+    {
+            if (size != toAssign.getSize())
+            {
+                delete [] internalArray;
+                size = toAssign.getSize();
+                internalArray = new Type [size];
+            }
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return *this;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* Array_hpp */
