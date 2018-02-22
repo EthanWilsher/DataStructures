c33b3e06026e47e18223ec77aa74185a25ea2a04
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-02-22 09:02:03 -0700

    Added  stack class and started working on  linked list tester

---
+++ DataStructuresProject/DataStructuresProject/Testers/LinkedListTester.hpp
@@ -0,0 +1,18 @@
+//
+//  LinkedListTester.hpp
+//  DataStructuresProject
+//
+//  Created by Wilsher, Ethan on 2/22/18.
+//  Copyright © 2018 Wilsher, Ethan. All rights reserved.
+//
+
+#ifndef LinkedListTester_hpp
+#define LinkedListTester_hpp
+
+#include "../Model/Structure/Linear/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+#include <stdio.h>
+
+#endif /* LinkedListTester_hpp */
